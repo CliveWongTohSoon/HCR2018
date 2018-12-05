cdbec24e87710ab823d8afa79cbb448c04dff8a5
NGJINYEE <36297105+NGJINYEE@users.noreply.github.com>
2018-12-05 21:06:09 +0000

    Add files via upload

---
+++ raspberry_client/client_loop.py
@@ -0,0 +1,33 @@
+from socketIO_client_nexus import SocketIO, LoggingNamespace
+from gpiozero import Motor
+from time import sleep
+
+cloud_url = 'http://ec2-35-176-128-102.eu-west-2.compute.amazonaws.com'
+local_url = 'localhost'
+socketIO=SocketIO(cloud_url, 9000, LoggingNamespace)
+
+motor1 = Motor(14,15)
+
+
+def box_status( *args):
+    if args[0] == "open":
+        print ("open")
+        motor1.forward()
+        #motor_pos.on()
+        #motor_neg.off()
+        sleep(2)
+    elif args[0] =="close":
+        print("close")
+        motor1.backward()
+        #motor_pos.off()  # for closing
+        #motor_neg.on()
+        sleep(2)
+    motor1.stop()
+    sleep(2)
+    #motor_pos.off()  # for stop
+    #motor_neg.off()
+
+socketIO.on('box', box_status)
+
+socketIO.wait()
+
