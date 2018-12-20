741a399b289d9875470f2f5cead4e30bc5242a36
clivewong21 <xtwc32@live.com>
2018-12-20 00:12:16 +0000

    updated link

--- mock_ROS/face_detect.py
+++ mock_ROS/face_detect.py
@@ -13,9 +13,9 @@ cwd = os.getcwd()
 
 class FaceSocket:
 	def __init__(self):
-		url = "http://ec2-35-176-128-102.eu-west-2.compute.amazonaws.com"
+		url = "http://ec2-52-56-122-124.eu-west-2.compute.amazonaws.com"
 		url_local = 'localhost'
-		self.socketIO = SocketIO(url_local, 9000, LoggingNamespace)
+		self.socketIO = SocketIO(url, 9000, LoggingNamespace)
 		self.facial_count = 0
 
 	def stream_video_to_socket(self, img):
