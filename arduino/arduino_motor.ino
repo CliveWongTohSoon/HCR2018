94fbe49f486f63d53c1c1802911a1d3c86d72b67
NGJINYEE <36297105+NGJINYEE@users.noreply.github.com>
2018-12-20 15:57:36 +0000

    Add files via upload

--- arduino/arduino_motor.ino
+++ arduino/arduino_motor.ino
@@ -1,1 +1,44 @@
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);          // sets the digital pin 9 as output
+  pinMode(10, OUTPUT); 
+  pinMode(12,INPUT);
+  pinMode(13,INPUT);
+}
 
+void loop()
+{
+  if (digitalRead(12) && !digitalRead(13)){
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);// sets the digital pin 13 on
+    //delay(1000);
+     Serial.print("High") ;
+  }
+  
+  else if (!digitalRead(12) && digitalRead(13)) {
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH);// sets the digital pin 13 on
+    //delay(1000);
+     Serial.print("Low") ;
+  }
+  
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  
+
+  //digitalWrite(9, HIGH);
+  //digitalWrite(10, LOW);// sets the digital pin 13 on
+  //delay(5000);                  // waits for a second
+  //digitalWrite(9, LOW);        // sets the digital pin 13 off
+  //digitalWrite(10, LOW);
+  //delay(1000);
+  //digitalWrite(9, LOW);       // sets the digital pin 13 on
+  //digitalWrite(10, HIGH); 
+  //delay(1000);                  // waits for a second
+  //digitalWrite(9, LOW);       // sets the digital pin 13 on
+  //digitalWrite(10, LOW); 
+  //delay(1000); 
+  
+  // waits for a second
+}
